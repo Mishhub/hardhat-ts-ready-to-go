b3461a9262bf77d5f1164dbdcc220b2b76bea29c
Mishhub <mishhub@gmail.com>
2023-03-29 05:39:48 +0400

    added deploy and verify contract scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,20 +1,40 @@
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
+  const simpleStorage = await SimpleStorage.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  await simpleStorage.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  // Verify contract on Etherscan
+  // We only verify on a testnet!
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    // 6 blocks is sort of a guess
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Deployed to ${simpleStorage.address} on network ${network.name} with ${network.config.chainId} chainId`
   );
 }
+
+const verify = async (contractAddress: string, args: any[]) => {
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  }
+  catch ( error: any ) {
+    if ( error?.message?.toLowerCase().includes('Contract source code already verified') ) {
+      console.log('Contract source code already verified');
+      return;
+    }
+    console.log(error);
+  }
+};
+
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
